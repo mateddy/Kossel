50aaee3ca54daa9ca2b59e38caf11b7205b463ef
James Lee <mateddy@gmail.com>
2015-10-08 01:14:36 +0800

    Adjustments using aluminium cornerplates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -90,7 +90,7 @@
 #define DELTA_DIAGONAL_ROD 214.0 // mm
 
 // Horizontal offset from middle of printer to smooth rod center.
-#define DELTA_SMOOTH_ROD_OFFSET 145.0 // mm
+#define DELTA_SMOOTH_ROD_OFFSET 150.36 // mm
 
 // Horizontal offset of the universal joints on the end effector.
 #define DELTA_EFFECTOR_OFFSET 19.9 // mm
